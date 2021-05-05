09c8ac81b221295f7d742bd7d1bcf16fbc59f8a3
Maulik <maulik@pardypanda.com>
2021-05-05 15:55:34 +0530

    for public access

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ const transporter = nodemailer.createTransport({
 schedule.scheduleJob("*/10 * * * *", async function () {
   const date = new Date();
   console.log(date);
-  const pincode = 364001;
-  const center_id = 599643;
+  const pincode = process.env.PINCODE;
   const options = {
     method: "GET",
     hostname: "cdn-api.co-vin.in",
@@ -43,35 +42,28 @@ schedule.scheduleJob("*/10 * * * *", async function () {
     res.on("end", function (chunk) {
       var body = Buffer.concat(chunks);
       let jsonData = JSON.parse(body.toString());
-
+      let html = "<h1>Available slots in your area:</h1>";
+      let data = ``;
       const centers = jsonData.centers;
-      const availability = centers
-        .filter((item) => item.center_id == center_id)[0]
-        .sessions.map((dates, i) => {
+      centers.map((center) => {
+        center.sessions.map((session) => {
           if (
-            dates.available_capacity > 0 &&
-            dates.min_age_limit >= 18 &&
-            dates.min_age_limit < 45
+            session.available_capacity > 0 &&
+            session.min_age_limit > 18 &&
+            session.min_age_limit < 45
           ) {
-            return dates.date;
+            data += `<p>Center: <strong>${center.name}</strong></p>`;
+            data += `<p>Address: <strong>${center.address}</strong></p>`;
+            data += `<p>Date: <strong>${session.date}</strong></p>`;
+            data += `<p>Capacity: <strong>${session.available_capacity}</strong></p><hr/>`;
           }
-        })
-        .filter((item) => item);
-
-      if (availability.length > 0) {
-        let html = `<h1>Slots are available for following dates at ${
-          centers.filter((item) => item.center_id == center_id)[0].name
-        }</h1>`;
-        for (let i = 0; i < availability.length; i++) {
-          html += `<p>${availability[i]}<p>`;
-        }
+        });
+      });
+      if (data) {
         let mailOptions = {
-          from: "maulik.sompura06@gmail.com",
-          to: [
-            "mauliksompura+zkyo3uj8czbq2ch3fw5d@boards.trello.com",
-            "maulik.sompura06@gmail.com",
-          ],
-          subject: "Slot Available",
+          from: process.env.EMAIL,
+          to: [process.env.TOEMAIL],
+          subject: "Slot Available - " + date.toLocaleDateString(),
           html: html,
         };
         transporter.sendMail(mailOptions, function (err, data) {
